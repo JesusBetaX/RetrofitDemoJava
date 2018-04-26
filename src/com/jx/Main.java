e1dc2f135b0756fa6f8c56a447a8d10af0a09b09
JesusBetaX <jjbaezmm@yahoo.com>
2018-04-26 15:03:54 -0500

    restructura

--- src/com/jx/Main.java
+++ src/com/jx/Main.java
@@ -14,7 +14,20 @@ public class Main {
     PokemonFeedDao dao = WebService.getDao(PokemonFeedDao.class);
     Call<PokemonFeed> call = dao.getData();
 
+    /*
+     * Envía de manera asíncrona la petición y notifica a tu aplicación con un 
+     * callback cuando una respuesta regresa. Ya que esta petición es asíncrona,
+     * Retrofit maneja la ejecución en el hilo de fondo para que el hilo de la 
+     * UI principal no sea bloqueada o interfiera con esta.
+     */
     call.enqueue(new Callback<PokemonFeed>() {
+      
+      /*
+       * Invocado para una respuesta HTTP recibida. Este método es llamado para 
+       * una respuesta que puede ser correctamente manejada incluso si el servidor 
+       * devuelve un mensaje de error. Así que si obtienes un código de estado 
+       * 404 o 500, este método aún será llamado. 
+       */
       @Override
       public void onResponse(Call<PokemonFeed> call, Response<PokemonFeed> response) {
         if (response.code() == 200) {
@@ -26,6 +39,11 @@ public class Main {
         }
       }
 
+      /**
+       * invocado cuando una excepción de red ocurre comunicando al servidor o 
+       * cuando una excepción inesperada ocurrió manejando la petición o 
+       * procesando la respuesta.
+       */
       @Override
       public void onFailure(Call<PokemonFeed> call, Throwable t) {
         t.printStackTrace(System.out);
@@ -36,6 +54,6 @@ public class Main {
   public static void main(String[] args) {
     Main obj = new Main();
     obj.loadJSON();
-    obj.loadJSON();
+    //obj.loadJSON();
   }
 }
